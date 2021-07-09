d7d00d56fbff401a095a5ad4d9a01f4e9acb91cb
Sunera Wickramasinghe <65078660+SuneraWickramasinghe@users.noreply.github.com>
2021-07-09 14:02:25 +0530

    recycling

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,9 +1,3 @@
-// import Navbar from './Navbar';
-// import SignIn from './SignIn';
-// import SignUp from './SignUp';
-// import AdminHome from './AdminHome';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 const Home = () => {
     return (
         <div className="home">
@@ -14,45 +8,3 @@ const Home = () => {
 }
 
 export default Home;
-
-// function App() {
-
-//   const [id, setId] = useState({});
-
-//   return (
-//     <BrowserRouter>
-//       <div className="App">
-//         <Navbar />
-//         {/* <div className="content">
-//           <Switch>
-//             <Route exact path="/">
-
-//             </Route>
-
-//             <Route path="/signIn">
-//               <SignIn setId={setId} />
-//             </Route>
-
-//             <Route path="/signUp">
-//               <SignUp />
-//             </Route>
-
-//             <Route path="/adminHome">
-//               <AdminHome id={id} />
-//             </Route>
-//           </Switch>
-//         </div> */}
-//         <div class="carousel-item">
-//   <img src="..." alt="...">
-//   <div class="carousel-caption d-none d-md-block">
-//     <h5>...</h5>
-//     <p>...</p>
-//   </div>
-// </div>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
