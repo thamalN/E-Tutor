33d041105bf6e8ddc266b341836a69f23d3f893c
thamalN <thamalnanayakkaraix@gmail.com>
2021-07-10 16:12:17 +0530

    Conditional navbar fixed / Log out

--- frontend/src/Admin/AdminHome.js
+++ frontend/src/Admin/AdminHome.js
@@ -1,15 +1,33 @@
-import { Link } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import Sidebar from "../Sidebar";
 
-const AdminHome = () => {
-    return ( <div>
-        <nav className="navbar">
-            <div className="links">
-                <Link to="/Admin/createTeacherAcc">Create Teacher Account</Link>
-                <Link to="/">Create Supporting Staff Account</Link>
-            </div>
-        </nav>
-        <Sidebar/></div>);
+const AdminHome = (props) => {
+
+    const history = useHistory()
+
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    const logOut = () => {
+        props.setLoggedIn(false)
+        localStorage.clear();
+        history.replace("/")
+    };
+
+    return ( 
+        <div>
+            <nav className="navbar">
+                <div className="links">
+                    <Link to="/Admin/createTeacherAcc">Create Teacher Account</Link>
+                    <Link to="/">Create Supporting Staff Account</Link>
+                </div>
+            </nav>
+
+            <Sidebar/>
+
+            <h1> User Id = {user.user_id } </h1>
+            <button onClick={logOut}>Log Out</button>
+        
+        </div>);
 }
  
 export default AdminHome;
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,46 +1,52 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import logo from './Resources/logo_white.png';
 
-const Navbar = () => {
+const Navbar = (props) => {
     let navbar;
     
-    let user = JSON.parse(localStorage.getItem('user'))
+    const location = useLocation()
 
-    if (!user) {
-        navbar = <div className="links">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/courses">Courses</Link>
-                    <Link to="/staff">Staff</Link>
-                    <Link to="/contact">Contact</Link>
-                    <Link to="/signIn">Sign In</Link>
-                    <Link className="joinForFree" to="/signUp">Join For Free</Link>
-                </div>
+    useEffect(() => {
+        const loggedUser = localStorage.getItem('user')
+        if (loggedUser)
+            props.setLoggedIn(true)
+    })
+
+    if (!(props.loggedIn)) {
+        navbar = <nav className="navbar">
+                    <div className="navbar-brand">
+                        <Link to="/">
+                            <img src={logo} className="rounded float-left" alt="eTutor" width="50%" />
+                        </Link>
+                    </div><div className="links">
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="/courses">Courses</Link>
+                        <Link to="/staff">Staff</Link>
+                        <Link to="/contact">Contact</Link>
+                        <Link to="/signIn">Sign In</Link>
+                        <Link className="joinForFree" to="/signUp">Join For Free</Link>
+                    </div>
+                </nav>
 
     } else {
-        navbar = <div className="links">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/courses">Courses</Link>
-                    <Link to="/staff">Staff</Link>
-                    <Link to="/contact">Contact</Link>
-                    <Link to="#">Username</Link>
-                </div>
+        navbar = <nav className="navbar">
+                    <div className="navbar-brand">
+                        <Link to= {location.pathname}>
+                            <img src={logo} className="rounded float-left" alt="eTutor" width="50%" />
+                        </Link>
+                    </div><div className="links">
+                        <Link to="">Username</Link>
+                    </div>
+                </nav>
 
     }
 
     return (
-
-        <nav className="navbar">
-            <div className="navbar-brand">
-                <Link to="/">
-                    <img src={logo} className="rounded float-left" alt="eTutor" width="50%" />
-                </Link>
-            </div>
-
-            {navbar}
-            
-        </nav>
+        <div>
+            { navbar }
+        </div>
     );
 }
 
