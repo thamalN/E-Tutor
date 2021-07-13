85cc69219025c4752ba7e6ed5ba5b898fbb6560c
Sunera Wickramasinghe <65078660+SuneraWickramasinghe@users.noreply.github.com>
2021-07-14 00:13:47 +0530

    calendar + stuff

---
+++ frontend/src/Calendar.js
@@ -0,0 +1,16 @@
+import './Resources/calendar.css';
+
+const Calendar = () => {
+    return ( 
+        <iframe 
+        src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FColombo&amp;src=Zzk2NDcuc3VuZXJhQGdtYWlsLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4ubGsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;showTitle=0&amp;showTabs=0&amp;showPrint=0" 
+        className="c1"
+        style={{border:'solid' + '1px' + '#777'}} 
+        width="400" 
+        height="400" 
+        frameBorder="0" 
+        scrolling="no"></iframe>
+     );
+}
+ 
+export default Calendar;
