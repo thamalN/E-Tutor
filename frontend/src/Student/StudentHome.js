cb2f20008586c46bb9af9237086df266ea53bb10
nuvinda <nuvindaravishan@gmail.com>
2021-07-14 06:01:55 +0530

    Updated studentHome

--- frontend/src/Student/StudentHome.js
+++ frontend/src/Student/StudentHome.js
@@ -21,6 +21,19 @@ const StudentHome = (props) => {
             <Sidebar />
 
             <div className="homeContent">
+                <h1>Recently Accessed Courses</h1>
+                <div className="courses">
+                    
+                            <div className="course-card-container">
+                                <div className="card-container">
+                                    <h1>Combined Mathematics 2023 A/L</h1>
+                                    <p>Pure Mathematics, Applied Mathematics</p>
+                                </div>
+
+                            </div>
+                    
+                </div>
+            
             <Calendar />
 
             </div>
