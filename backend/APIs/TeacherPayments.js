72f90f97314233b5998fc8b33e06661e273b9cff
nuvinda <nuvindaravishan@gmail.com>
2021-09-19 20:45:09 +0530

    added logo to pdf

--- backend/APIs/TeacherPayments.js
+++ backend/APIs/TeacherPayments.js
@@ -36,7 +36,7 @@ module.exports = function (app, db, MySQLEvents, schedule) {
     //     }, {})
     //   }
 
-    const job = schedule.scheduleJob("22 11 14 * *", function () {
+    const job = schedule.scheduleJob("23 20 19 * *", function () {
 
         const query = "SELECT course.*, teacher.*, user.fname, user.lname, user.email, user.contact, user.street_no, user.street, user.city, user.province, SUM(payment.amount)*0.6 AS amount_payable FROM course RIGHT JOIN teacher ON teacher.teacher_id=course.teacher_id  LEFT JOIN user ON user.user_id=teacher.teacher_id LEFT JOIN payment ON payment.course_id=course.course_id AND payment.verified=1 AND payment.month=MONTHNAME(NOW()) GROUP BY course.course_id HAVING course.course_id IS NOT NULL;"
 
@@ -92,6 +92,7 @@ module.exports = function (app, db, MySQLEvents, schedule) {
         })
         var dd = {
               content: [
+              {image: 'Content/logo_black.png', width: 150, height: 80},
               {text: 'ETutor - Teacher Payment Info - '+ this_month, style: 'header'},
               {text: 'Teacher Info', style: 'subheader'},
               
