f8b9f3bb01efeebc9f7711381d503af447d02600
thilinadi <hbthilinadilshan123@gmail.com>
2021-09-21 02:17:39 +0530

    small changes

--- frontend/src/Student/StuFeedback.js
+++ frontend/src/Student/StuFeedback.js
@@ -6,11 +6,10 @@ const stuFeedback = () => {
         <div>
             <Sidebar/>
             <div className="homeContent">
-            <h1>FeedBacks</h1>
+            <h1>FeedBack</h1>
             <div className="reg_buttons">
             <Link to="/studentHome/CreateStuFeedback">Add new Feedback</Link>
             <Link to="/studentHome/PreviousFeedback">View previous Feedback</Link>
-            <Link to="/studentHome">Back to Home</Link>
         </div>
         </div>
         </div>
