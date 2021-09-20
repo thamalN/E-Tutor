e9d7fc331d501d221322c3ff1f8672912fbc49ff
Thishan96 <2018cs039@stu.ucsc.cmb.ac.lk>
2021-09-20 17:47:45 +0530

    new commit payment of courses and sql quaries added for filter paied courses

--- backend/APIs/StudentNotification.js
+++ backend/APIs/StudentNotification.js
@@ -1,38 +1,38 @@
 module.exports = function (app, db, MySQLEvents, schedule) {
 
-    const instance = new MySQLEvents(db, {
-        startAtEnd: true
-    });
+    // const instance = new MySQLEvents(db, {
+    //     startAtEnd: true
+    // });
 
-    instance.start();
+    // instance.start();
 
-    var notifications = []
+    // var notifications = []
 
-    instance.addTrigger({
-        name: 'INSERT CONTENT',
-        expression: 'etutor.content',
-        statement: MySQLEvents.STATEMENTS.INSERT,
-        onEvent: (event) => {
-            console.log(event);
-            let lesson_id = event.affectedRows[0].after.lesson_id
-            let content = event.affectedRows[0].after.content
+    // instance.addTrigger({
+    //     name: 'INSERT CONTENT',
+    //     expression: 'etutor.content',
+    //     statement: MySQLEvents.STATEMENTS.INSERT,
+    //     onEvent: (event) => {
+    //         console.log(event);
+    //         let lesson_id = event.affectedRows[0].after.lesson_id
+    //         let content = event.affectedRows[0].after.content
 
-            const query = "SELECT DISTINCT course.course_name, course.year FROM ((course RIGHT JOIN lesson ON course.course_id = lesson.course_id) LEFT JOIN content ON lesson.lesson_id = content.lesson_id) WHERE lesson.lesson_id=?;"
+    //         const query = "SELECT DISTINCT course.course_name, course.year FROM ((course RIGHT JOIN lesson ON course.course_id = lesson.course_id) LEFT JOIN content ON lesson.lesson_id = content.lesson_id) WHERE lesson.lesson_id=?;"
 
-            db.query(query, lesson_id, (err, result) => {
-                if (err) throw err;
-                console.log(result)
-                notifications.push({ event: "content", content: content, course_name: result[0].course_name, year: result[0].year })
-            })
+    //         db.query(query, lesson_id, (err, result) => {
+    //             if (err) throw err;
+    //             console.log(result)
+    //             notifications.push({ event: "content", content: content, course_name: result[0].course_name, year: result[0].year })
+    //         })
 
-        },
-    });
+    //     },
+    // });
 
     let dueStudents = []
 
     const date = new Date(2021, 08, 20, 15, 39, 10);
 
-    const job = schedule.scheduleJob(date, function () {
+    const job = schedule.scheduleJob("00 00 21 * *", function () {
 
         const query = "SELECT payment.student_id, course.course_name, course.year from payment INNER JOIN course ON payment.course_id=course.course_id WHERE month != MONTHNAME(NOW()) GROUP BY payment.student_id, payment.course_id, payment.month;"
 
