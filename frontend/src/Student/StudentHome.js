29272a2ff165e7bf0b32213fa9197cf1bb257130
Sunera Wickramasinghe <65078660+SuneraWickramasinghe@users.noreply.github.com>
2021-09-21 18:58:00 +0530

    studenthome

--- frontend/src/Student/StudentHome.js
+++ frontend/src/Student/StudentHome.js
@@ -4,6 +4,12 @@ import { useHistory } from "react-router";
 import { Link, Route } from 'react-router-dom';
 // import EditIcon from '@material-ui/icons/Edit';
 // import DeleteIcon from '@material-ui/icons/Delete';
+import '../SupportingStaff/staffhome.css';
+import pencil1 from '../Resources/pencil1.jpg';
+import pencil2 from '../Resources/pencil2.jpg';
+import pencil3 from '../Resources/pencil3.jpg';
+import pencil4 from '../Resources/pencil4.jpg';
+import Card2 from "../Card2.js";
 
 const StudentHome = () => {
 
@@ -23,7 +29,7 @@ const StudentHome = () => {
             headers: { "Content-Type": "application/json" },
             credentials: 'include',
             body: JSON.stringify(id)
-        }).then(res =>  {
+        }).then(res => {
             return res.json()
         }).then((data) => {
             console.log(data)
@@ -45,81 +51,125 @@ const StudentHome = () => {
             }))
     }, [del])
 
-//     const handleEdit = (announcement) =>{
-//         console.log(announcement.attachment)
-//         localStorage.setItem('announce', JSON.stringify(announcement))
-//         history.push("/studentHome/announcements/editAnnouncement")
+    //     const handleEdit = (announcement) =>{
+    //         console.log(announcement.attachment)
+    //         localStorage.setItem('announce', JSON.stringify(announcement))
+    //         history.push("/studentHome/announcements/editAnnouncement")
 
-// }
+    // }
 
-// const handleDelete = (key) => {
-//     if (window.confirm("Are you sure you want to delete announcement id " + key + "?")) {
-//         const id = { id: key }
-//     const url3 = "http://localhost:3001/deleteAnnouncement"
+    // const handleDelete = (key) => {
+    //     if (window.confirm("Are you sure you want to delete announcement id " + key + "?")) {
+    //         const id = { id: key }
+    //     const url3 = "http://localhost:3001/deleteAnnouncement"
 
-//         fetch(url3, {
-//             method: 'POST',
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify(id)
-//     })
-//             .then((res => {
-//                 return res.json()
-//             }))
-//             .then((data => {
+    //         fetch(url3, {
+    //             method: 'POST',
+    //             headers: { "Content-Type": "application/json" },
+    //             body: JSON.stringify(id)
+    //     })
+    //             .then((res => {
+    //                 return res.json()
+    //             }))
+    //             .then((data => {
 
-//                 if (data.status === "ok") {
-//                     setDel(data)
-//                     alert("Announcement deleted Successfully!")
-//                     history.push("/studentHome/studentAnnouncements")
-                    
-//                   } 
-//                   else {
-//                     alert("Sorry the task couldn't be completed");
-//                     history.push("/studentHome/studentAnnouncements")
-                    
-//                   }
-                
-//             }))
-//         }
-        
-    
-// }
+    //                 if (data.status === "ok") {
+    //                     setDel(data)
+    //                     alert("Announcement deleted Successfully!")
+    //                     history.push("/studentHome/studentAnnouncements")
+
+    //                   } 
+    //                   else {
+    //                     alert("Sorry the task couldn't be completed");
+    //                     history.push("/studentHome/studentAnnouncements")
+
+    //                   }
+
+    //             }))
+    //         }
+
+
+    // }
+
+    const [courses, setCourses] = useState()
+    const url3 = "http://localhost:3001/studentHome/courses"
+
+    useEffect(() => {
+
+        fetch(url3, {
+            credentials: 'include'
+        })
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setCourses(data)
+            })
+
+    }, [url3])
+
+    const [quizzes, setQuizzes] = useState()
+    const url4 = "http://localhost:3001/studentHome/quizzes"
+
+    useEffect(() => {
+
+        fetch(url4, {
+            credentials: 'include'
+        })
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setQuizzes(data)
+            })
+
+    }, [url4])
+
     return (
         <div>
-            <Sidebar/>
+            <Sidebar />
             <div className="homeContent">
-            
-            <h1>Announcements</h1>
-            <div className="courses">
+
+                <div className="title">
+                    <h2>Welcome back, {user.fname}  {user.lname} !</h2>
+                </div>
+
+                <div className="wrapper">
+                    <Card2 title="My Courses" description={courses} image={pencil1}></Card2>
+                    <Card2 title="Upcoming Quizzes" description={quizzes} image={pencil2}></Card2>
+                </div>
+
+                <h1>Announcements</h1>
+                <div className="courses">
                     {data.map(announcement => (
                         <div key={announcement.announcement_id} className="display-card" >
                             <div className="display-card-container">
                                 <div className="n-card-container">
-                                <div  className="download_icons">
-                           
-                            </div>
-                                
+                                    <div className="download_icons">
+
+                                    </div>
+
                                     <h1>{announcement.topic}</h1>
                                     <h6> By {announcement.fname} {announcement.lname} {announcement.date_time}</h6>
                                     <div><p>{announcement.description}</p></div>
-                                    
+
                                     <a href={announcement.attachment} target="_blank" rel="noreferrer">
-                                    
-                                    {announcement.attachment && <div>{announcement.file_name}</div>}
-                                        
-                                        </a>
-                                        
+
+                                        {announcement.attachment && <div>{announcement.file_name}</div>}
+
+                                    </a>
+
                                 </div>
                             </div>
 
                         </div>
                     ))}
                 </div>
-            
+
+            </div>
         </div>
-        </div>
-        
-      );
+
+    );
 }
- 
+
 export default StudentHome;
